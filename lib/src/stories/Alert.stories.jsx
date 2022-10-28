614920c044de056ca5014f011946a00f3a0d2501
Francis Bolduc <f.bolduc@outlook.com>
2022-10-28 01:23:26 -0400

    ss2

---
+++ lib/src/stories/Alert.stories.jsx
@@ -0,0 +1,28 @@
+import './style.css'
+
+import { Alert } from '../components/Alert/Alert';
+
+export default {
+  title: 'Components-TP1/Alert',
+  component: Alert
+};
+
+const Template = (args) => <Alert {...args} />;
+
+export const Success = Template.bind({});
+Success.args = {
+    text: "Ceci est une texte de Success",
+    variant: "success",
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+    text: "Ceci est un texte de Warning",
+    variant: "warning",
+};
+
+export const Info = Template.bind({});
+Info.args = {
+    text: "Ceci est une Information",
+    variant: "info",
+};
---
+++ lib/src/stories/Avatar.stories.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './style.css'
+
+import { Avatar } from '../components/Avatar/Avatar';
+
+export default {
+  title: 'Components-TP1/Avatar',
+  component: Avatar
+};
+
+const Template = (args) => <Avatar {...args} />;
+
+export const Orange = Template.bind({});
+Orange.args = {
+  color: "#f56a00",
+  text: 'Bill',
+  size: "small"
+};
+
+export const Purple = Template.bind({});
+Purple.args = {
+    color: "#800080",
+    text: 'Max',
+    size: "medium"
+};
+
+export const Red = Template.bind({});
+Red.args = {
+    color: "#FF0000",
+    text: 'Ginette',
+    size: "large"
+};
