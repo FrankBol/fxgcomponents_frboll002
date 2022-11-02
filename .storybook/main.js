54ccca2cf771a644feb92ccb8b129604e6f57a60
Francis Bolduc <f.bolduc@outlook.com>
2022-11-01 20:06:59 -0400

    q

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,13 @@
 module.exports = {
   stories: [
-    "../**/*.stories.@(js|jsx|ts|tsx)"
+    // "../lib/src/**/*.stories.mdx",
+    "../lib/src/**/*.stories.@(js|jsx|ts|tsx)"
+    // "../**/*.stories.@(js|jsx|ts|tsx)"
   ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
   framework: "@storybook/react",
-
+  staticDirs: [{ from: '../lib/src/assets', to: '/assets' }]
 };
